c81b3fbc7b51eded428ec7b18f2c851901d2c3b4
Tojo-glitch <chanon.chonv@gmail.com>
2025-12-26 12:29:36 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,1 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(
-    caches.open('v1').then(cache=>{
-      return cache.addAll(['/index.html','/style.css','/script.js']);
-    })
-  );
-});
 
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
-  );
-});
