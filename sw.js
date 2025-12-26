48fbebbd9c50f0b5eccc8387fbe9a168d871bb5f
Tojo-glitch <chanon.chonv@gmail.com>
2025-12-26 11:43:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', event => { /* cache strategy can be added */ });
+
