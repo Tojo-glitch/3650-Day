0bc00bd526cf34c4a55515cb056f3aec876af97d
Tojo-glitch <chanon.chonv@gmail.com>
2025-12-26 12:04:09 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,12 @@
-const CACHE_NAME = 'anniversary-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('v1').then(cache=>{
+      return cache.addAll(['/index.html','/style.css','/script.js']);
+    })
+  );
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request))
+  );
 });
